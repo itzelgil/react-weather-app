f8948ca28e39b5692cf910889eabe56b0bc88c0b
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-05-09 19:23:06 +0200

    Added styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,37 +4,42 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Entera city..."
-              className="form-control"
-            />
-          </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-        </div>
-      </form>
-      <h1>Barcelona</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
       <div className="row">
         <div className="col-6">
-          <span>⛅️</span>
-          {""}
-          6ºC
+          <h1>BARCELONA</h1>
+          <p>Monday | May 9, 2022 | 18:22</p>
         </div>
         <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity:72%</li>
-            <li>Wind: 13km/h</li>
-          </ul>
+          <form className="d-flex">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </form>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <h2 className="icon">⛅️</h2>
+          <h3>Few Clouds</h3>
+        </div>
+        <div className="col-6">
+          <span className="currentTemp">21</span>
+          <span className="degrees">ºC</span>
+
+          <h5>Feels like 21ºC</h5>
+          <p>H: 26º L: 19º</p>
+        </div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col-12">
+          <p>Humidity: 60%</p>
+          <p>Wind speed: 4 km/h</p>
         </div>
       </div>
     </div>

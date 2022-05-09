e0824c18fc5ade6098e894d0298ccdf2c1ac12ea
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-05-09 17:39:02 +0200

    adding bootstrap inside index.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://ubiquitous-gnome-f8865b.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Itzel Gil{" "}
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/itzelgil/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://ubiquitous-gnome-f8865b.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Itzel Gil{" "}
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/itzelgil/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

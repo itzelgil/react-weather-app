8fc335d2804c0c278760ba7fa4e71d66883d27fa
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-05-09 16:55:40 +0200

    Added footer Github URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://ubiquitous-gnome-f8865b.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Itzel Gil{" "}
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/itzelgil/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
